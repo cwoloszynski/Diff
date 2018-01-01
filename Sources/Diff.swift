3f6f0a56c6551b8669542b1b9656334471269ed2
Charlie Woloszynski <charles.woloszynski@handheldmedia.us>
2018-01-01 10:49:04 -0500

    - Update documentation of the function design to better understand the logic.

--- Sources/Diff.swift
+++ Sources/Diff.swift
@@ -30,20 +30,25 @@ public func diff<T>(_ before: [T], _ after: [T], compare: (T, T) -> Bool) -> (Co
 	while commonEnd + commonStart < beforeCount && commonEnd + commonStart < afterCount && compare(before[beforeCount - 1 - commonEnd], after[afterCount - 1 - commonEnd]) {
 		commonEnd += 1
 	}
+    
+    // The return of this function is the range of blocks in the 'before' that were affected and
+    // the list of the items that belong in that range to replace those affected.
+    // [e.g. Delete the 'range' then insert the 'affected' items to apply the changes]
 
-	// Remove
+	// The structure 'shrunk' since the count before and the common set after are not the same.
+    // [Note: Common start + common end must always be less than or equal to the full count]
+    // Note:  shrink is different than removal, since you can shrink the structure and still both remove and insert.
 	if beforeCount != commonStart + commonEnd {
 		let range = commonStart..<(beforeCount - commonEnd)
 		let intersection = commonStart..<(afterCount - commonEnd)
 		return (range, Array(after[intersection]))
-	}
-
-	// Insert
-	if afterCount != commonStart + commonEnd {
+	}   // The structure 'grew' since there are more blocks at the end than there were in the beginning and the 'shrink' was not detected.
+        // In this case, the identified blocks are 'insertions'
+	else if afterCount != commonStart + commonEnd {
 		let range = commonStart..<(afterCount - commonEnd)
 		return (commonStart..<commonStart, Array(after[range]))
-	}
-
-	// Already equal
-	return nil
+    } else {
+        // Already equal
+        return nil
+    }
 }
